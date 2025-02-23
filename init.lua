5300984ac2ea0b8adf977b6a3fa3aac4a086b45e
Timurkaaa <timurzarovskiy@gmail.com>
2025-02-23 21:39:43 +0300

    feat: Create init.lua

---
+++ init.lua
@@ -0,0 +1,17 @@
+--NOTE: Basic
+require("config.lazy")
+require("config.configs")
+require("config.colors")
+require("config.mappings")
+
+--NOTE: Plugin
+require("plugins.dashboard")
+require("plugins.bufferline")
+require("plugins.todo")
+require("plugins.cmp")
+require("plugins.nvim-colorizer") 
+require("plugins.nvim-lspconfig")
+require("plugins.lualine")
+require("plugins.nvim-treesitter")
+
+
